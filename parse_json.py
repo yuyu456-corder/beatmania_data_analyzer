b96e6b6253c4ee4a0e8af793b6df59a655ce808a
yuyu456 <youq27668@gmail.com>
2020-03-23 17:50:53 +0900

    first commit: プロジェクトに必要なファイルをコミット

---
+++ parse_json.py
@@ -0,0 +1,49 @@
+# 必要なライブラリのインポート
+import pandas as pd
+
+# CSVファイルの読み込み
+my_play_data_flame = pd.read_csv(
+    "./beatmaniaPlayData.csv")
+
+# 読み込んだCSVファイルのdata_flameのデータ長を取得
+data_length = len(my_play_data_flame.index)
+
+# 読み込んだCSVで欲しい列のヘッダ名を設定する
+# このデータを使って分散などの統計データを取る予定
+music_title = my_play_data_flame["タイトル"]
+
+hyper_difficulty = my_play_data_flame["HYPER 難易度"]
+hyper_ex_score = my_play_data_flame["HYPER EXスコア"]
+hyper_misscount = my_play_data_flame["HYPER ミスカウント"]
+hyper_cleartype = my_play_data_flame["HYPER クリアタイプ"]
+hyper_dj_level = my_play_data_flame["HYPER DJ LEVEL"]
+
+another_difficulty = my_play_data_flame["ANOTHER 難易度"]
+another_ex_score = my_play_data_flame["ANOTHER EXスコア"]
+another_miss_count = my_play_data_flame["ANOTHER ミスカウント"]
+another_cleartype = my_play_data_flame["ANOTHER クリアタイプ"]
+another_dj_level = my_play_data_flame["ANOTHER DJ LEVEL"]
+
+latest_play_dateTime = my_play_data_flame["最終プレー日時"]
+play_count = my_play_data_flame["プレー回数"]
+
+# 各曲全て参照する
+# ここで曲（行）ごとのデータが取得できる
+for index in range(data_length):
+    print("Now Index is: %d " % index)
+    # ヘッダ部分はループ処理から除外する
+    if index == 0:
+        continue
+    # 稼働中バージョンでプレイ済みの曲のみ抽出
+    if play_count[index] > 0:
+        print("Title: %s " % music_title[index])
+        print("HYPER EX score: %s " % hyper_ex_score[index])
+        print("ANOTHER EX score: %s " % another_ex_score[index])
+        print("HYPER Cleartype: %s " % hyper_cleartype[index])
+        print("ANOTHER Cleartype: %s \n" % another_cleartype[index])
+
+# JSONファイルとして変換し出力する
+# force_asciiで2バイト文字のエスケープを回避する
+my_play_data_flame.to_json("./myPlayData.json", force_ascii=False)
+
+print("JSON data Creating Done!")
